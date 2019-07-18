6d04f8626454f2b42f7e51ed86e7f58c1690ee6d
freedomtolive <liutao402955169@qq.com>
2019-07-18 10:10:51 +0800

    ls2

--- node/cli/README.md
+++ node/cli/README.md
@@ -30,40 +30,68 @@
 	.parse(argv: string[])
 		解析执行传入的 argv 命令字符串，通常改命令字符串来自用户在命令行的输入，process.argv
 		commander 同时会默认创建一个 -h, --help 的选项
-    .version(str, flags?)
+	.version(str, flags?)
 		设置版本信息，该方法会自动为命令注册一个 -V,  --version 的 option
 		str：版本号
 		flags：指定的 option，默认为：”-V, --version”
-    .option(flags, description?, fn?, defaultValue?)
+	.option(flags, description?, fn?, defaultValue?)
 		设置命令选项
 		flags：选项标记名称，”-v, --version”
 		description：选项使用说明
 		fn：默认值，函数返回值为defaultValue，优先级高于defaultValue
 		defaultValue：选项默认值，如果需要的话
-    选项属性
+	选项属性
 		flags 中的格式可以接收参数
 		-n, --name [val]
 		-n, --name <val>
 			[] 可选
 			<> 必填
 		设置成功以后，会在命令对象下增加一个与全局的同名的属性
-    .action(fn)
+	.action(fn)
 		指定命令要执行的动作行为
 		该函数执行过程会接收到至少一个参数
 		如果命令中带有参数，则是对应的参数列表
 		参数的最后一个永远都是 commander 实例
-    .command(name, desc?, opts?)
+	.command(name, desc?, opts?)
 		子命令
 		name：命令的名称，也可以接受值
 			'create [appName]'
 		desc：简介
 		opts：配置
-    .description(str)
+	.description(str)
 		命令描述
 	.alias(str)
 		设置命令别名(命令简写)
 	.usage(str)
 		设置或获取当前命令的使用说明
+
+
+####chalk
+	安装
+		npm i chalk / yarn add chalk
+
+	使用
+		const chalk = require('chalk')
+		得到一个 chalk 对象，通过这个对象，我们就可以给控制台中的文字加上各种样式了，就像css一样
+
+	语法
+		chalk.<style>[.<style>...](string, [string...])
+
+		Styles
+			Modifiers 文字修饰：
+			bold Colors 文字颜色：red、green、yellow、blue、cyan 
+			Background colors 背景颜色：bgRed、bgGreen、bgYellow、bgBlue、bgCyan
+		自己写样式值
+			Colors
+				.hex('#DEADED') 
+				.keyword('orange') 
+				.rgb(15, 100, 204)
+			Background colors
+				.hex('#DEADED')
+				.keyword('orange')
+				.rgb(15, 100, 204)	
+
+	
 
 
 
---
+++ node/ls/ls2.js
@@ -0,0 +1,73 @@
+/**
+ * ls
+ *  输出当前运行命令所在的目录下的文件和文件夹
+ * ls -p d:\
+ *  我们还可以指定要显示的目录
+ */
+
+//  加载commander模块
+const commander = require("commander");
+
+// 设置当前命令的版本
+commander.version("1.0.0","-v, --version");
+
+// 添加文件夹模块
+const fs = require("fs");
+
+// 美化命令行
+const chalk = require('chalk');
+
+// 美化命令行实例
+//注：文字的修饰：斜体，加粗重windows下（cmd）下支持不是特别的好
+//console.log(chalk.italic('Hello world!') + ' miaov');
+// console.log(chalk.bold.red.bgGreen('Hello world!') + ' miaov');
+
+// 设置命令选项(-p)
+commander.option("-p , --path [path]" , "设置要显示的目录" , __dirname);
+
+// 以列表的形式显示(如果选项不接收用户输入的值，那么这个选项将以boolean的形式提供给后面命令使用)
+// 即用户输入值地时候为true，用户不输入值时为false
+commander.option('-l, --list', '以列表的形式显示');
+
+// 实现命令的具体逻辑
+commander.action(() => {  //这里的path参数就是在命令中定义的<path>
+    try{
+        // 读取文件夹
+        const files = fs.readdirSync( commander.path );
+        
+        console.log(commander.list);
+        if(commander.list){
+            // 用户输入内容时，以列表的形式显示
+            // 数组地map方法，返回一个新数组，file为数组中的每一项
+            let outPut = files.map( file => {
+
+                // 文件的扩展信息（除了文件内容以外的信息）
+                let stat = fs.statSync( commander.path + '/' + file );
+
+                return stat.isDirectory() ? chalk.red(`[目录]   ${file}\r\n`) : `[文件]   ${file}\r\n`;
+            }).join("");
+
+            console.log(outPut);
+
+        }else{
+            // 用户没有输入内容时，输出文件所在目录地数组
+            console.log(files);
+
+        }
+    }
+    catch(e){
+        // 开发过程中，可以把错误打印出来，实际发布以后应该屏蔽错误信息
+        console.log(e);
+    }
+})
+
+
+// 在把process.argv交给parse解析之前进行一个简单的处理，少于3个参数，表示使用的是默认值
+if (process.argv.length < 3) {
+    process.argv.push(__dirname);
+}
+// console.log(process.argv);
+
+
+// 输出
+commander.parse( process.argv );
