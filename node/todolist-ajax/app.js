5df85ea35f72ea1e4e07876f7f6eeb8d626ff39d
freedomtolive <liutao402955169@qq.com>
2020-05-09 17:36:29 +0800

    todolist-ajax

--- node/todolist-ajax/app.js
+++ node/todolist-ajax/app.js
@@ -1,9 +1,11 @@
 const Koa = require("koa");
 const Router = require('koa-router');
 const koaStaticCache = require('koa-static-cache');
-const Swig = require('koa-swig');
-const co = require('co');
-const bodyParser = require('koa-bodyparser');
+const BodyParser = require('koa-bodyparser');
+const fs = require('fs');
+
+
+let datas = JSON.parse(fs.readFileSync('./data/data.json'));
 
 const app = new Koa();
 
@@ -25,6 +27,9 @@ app.use( koaStaticCache( __dirname + '/static' , {
     gzip : true     
 }));
 
+// body 解析
+app.use( BodyParser() );
+
 /**
  * 接收响应
  */
@@ -32,6 +37,86 @@ const router = new Router();
 
 router.get('/', async ctx => {
     ctx.body = 'hello';
+})
+
+router.get('/todos', async ctx =>{
+    ctx.body = {
+        code: 0,
+        data: datas.todos
+    };
+})
+
+// 切换选中状态
+router.post('/toggle', async ctx => {
+    // 获取post提交的id值;
+
+    let id = ctx.request.body.id || 0;
+
+    if(!id){
+        ctx.body = {
+            code: 1,
+            data: '请传入id'
+        }
+    }else{
+        let todo = datas.todos.find( todo => todo.id == id);
+
+        todo.done = !todo.done;
+
+        ctx.body = {
+            code : 0,
+            data : todo
+        }
+    
+        fs.writeFileSync('./data/data.json', JSON.stringify(datas));
+    }
+})
+
+// 删除
+router.post('/remove', async ctx => {
+    let id = ctx.request.body.id || 0;
+    if(!id){
+        ctx.body = {
+            code: 1,
+            data: '请传入id'
+        }
+    }else{
+        datas.todos = datas.todos.filter( todo => todo.id != id );
+
+        ctx.body = {
+            code: 0,
+            data: '删除成功'
+        }
+
+        fs.writeFileSync('./data/data.json', JSON.stringify(datas));
+    }
+
+})
+
+
+// 添加
+router.post('/add', async ctx => {
+    // 接受title字段
+    let title = ctx.request.body.title || 0;
+    if(!title){
+        ctx.body = {
+            code: 1,
+            data: '请传入任务标题'
+        }
+    }else{
+        let newTask = {
+            id : ++datas._id,
+            title : title
+        }
+        datas.todos.push(newTask);
+
+        ctx.body = {
+            code: 0,
+            data: newTask
+        }
+
+        fs.writeFileSync('./data/data.json', JSON.stringify(datas));
+    }
+
 })
 
 app.use( router.routes() );
