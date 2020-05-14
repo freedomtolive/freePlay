cd9dc23cc18babd42aafbfd933b45446b12a2194
freedomtolive <liutao402955169@qq.com>
2020-05-14 13:07:17 +0800

    mysql

--- node/mysql/mysql-demo/app.js
+++ node/mysql/mysql-demo/app.js
@@ -31,11 +31,52 @@
         //     {id: 3, title: '学习mysql', done: false}]
         // };
 
-        const [data] = await connection.query("SELECT id,title,done FROM todos ORDER BY done DESC");
+        // 按照是否完成倒序排，按照id值倒序排（从大到小）
+        // const [data] = await connection.query("SELECT id,title,done FROM todos ORDER BY done DESC, id DESC");
+
+
+        /**
+         * 排序
+         * ORDER BY
+         * 
+         * 
+         * 查询数量限制
+         * LIMIT - top 10
+         * 查询偏移
+         * OFFSET
+         * 
+         * 分页:
+         *  把一定的数据按照每页固定的条数去显示，我们需要首先定义每页显示多少
+         * 
+         * 每页显示3条
+         *  1 : 0 - 2
+         *  2 : 3 - 5
+         *  3 : 6 - 8
+         * 每页显示 -> LIMIT
+         * 当前的页码 -> OFFSET
+         * 
+         * 如果页码从1开始算，那么对应的记录应该    LIMIT 3 OFFSET (页码-1 * 3)
+         * 
+         * 总页码
+         */
+
+
+        // 只查询两条数据（只展示两条并且根据id倒序排序）
+        // const [data] = await connection.query("SELECT id,title,done FROM todos ORDER BY id DESC LIMIT 2");
+
+        
+        // 查询3条数据，并且从第(page - 1) * 3开始查询（分页的基本原理）
+        const page = 1; //第几页
+        const prepage = 4; //每页显示几条
+        const [todos] = await connection.query(`SELECT id,title,done FROM todos ORDER BY id DESC LIMIT ${prepage} OFFSET ${(page - 1) * prepage}`) ;
 
         ctx.body = {
             code: 0,
-            data
+            data : {
+                page,
+                prepage,
+                todos
+            }
         }
     });
 
